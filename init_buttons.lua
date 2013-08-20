184c07e4d6a2858abcaadd6c13674c1769ce6875
radamanthus <rad@batnag.org>
2013-08-20 22:55:47 +0800

    App skeleton from muning

---
+++ init_buttons.lua
@@ -0,0 +1,55 @@
+_G.buttons = {
+  about = {
+    defaultSrc = "images/btn_blank.png",
+    defaultX = 160,
+    defaultY = 32,
+    overSrc = "images/btn_blank_over.png",
+    overX = 160,
+    overY = 32,
+    id = "btnAbout",
+    text = "ABOUT",
+    font = "Helvetica",
+    textColor = { 0, 0, 0, 255 },
+    emboss = false
+  },
+  help = {
+    defaultSrc = "images/btn_blank.png",
+    defaultX = 160,
+    defaultY = 32,
+    overSrc = "images/btn_blank_over.png",
+    overX = 160,
+    overY = 32,
+    id = "btnHelp",
+    text = "HELP",
+    font = "Helvetica",
+    textColor = { 0, 0, 0, 255 },
+    emboss = false
+  },
+  start = {
+    defaultSrc = "images/btn_blank.png",
+    defaultX = 160,
+    defaultY = 32,
+    overSrc = "images/btn_blank_over.png",
+    overX = 160,
+    overY = 32,
+    id = "btnStart",
+    text = "START",
+    font = "Helvetica",
+    textColor = { 0, 0, 0, 255 },
+    emboss = false
+  },
+  settings = {
+    defaultSrc = "images/btn_blank.png",
+    defaultX = 160,
+    defaultY = 32,
+    overSrc = "images/btn_blank_over.png",
+    overX = 160,
+    overY = 32,
+    id = "btnSettings",
+    text = "SETTINGS",
+    font = "Helvetica",
+    textColor = { 0, 0, 0, 255 },
+    emboss = false
+  }
+}
+
