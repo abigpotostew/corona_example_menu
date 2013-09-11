55fdd9ef74ccd7b2fe9b67004826c81e2e541e64
Radamanthus Batnag <rad@batnag.org>
2013-09-11 23:54:14 +0800

    Fix 8_4_show_sections

--- menu.lua
+++ menu.lua
@@ -10,6 +10,9 @@ local radlib = require "scripts.lib.radlib"
 local screen = nil
 
 local txtHeader = nil
+local txtSectionHeaders = {}
+local txtItemTitles = {}
+local imgItemThumbnails = {}
 ---------------------------------------------------------------------------------
 -- END OF VARIABLE DECLARATIONS
 ---------------------------------------------------------------------------------
@@ -23,6 +26,58 @@ local renderHeader = function( menu )
   screen:insert( txtHeader )
 end
 
+local renderSectionHeader = function( theme, section, sectionIndex )
+  local txtSectionHeader = display.newText( section.title, 0, 0, menu.theme.body.font, 24 )
+  local x = 10 + txtSectionHeader.width/2
+  local y = 60 + ((sectionIndex-1) * 316) + txtSectionHeader.height/2
+  txtSectionHeaders[#txtSectionHeaders + 1] = txtSectionHeader
+  txtSectionHeader.x = x
+  txtSectionHeader.y = y
+  local color = theme.body.color
+  txtSectionHeader:setTextColor( color.red, color.green, color.blue )
+  screen:insert( txtSectionHeader )
+end
+
+local renderSectionItems = function( theme, section, sectionIndex )
+  local color = theme.body.color
+  item1 = section.items[1]
+  item2 = section.items[2]
+
+  local img1 = display.newImageRect( item1.thumbnail.filename, system.DocumentsDirectory, item1.thumbnail.width, item1.thumbnail.height )
+  img1.x = 202
+  img1.y = 120 + ((sectionIndex-1) * 316) + img1.height/2
+  img1.item = item1
+  screen:insert( img1 )
+  imgItemThumbnails[#imgItemThumbnails + 1] = img1
+
+  local txt1 = display.newText( item1.title, 0, 0, menu.theme.body.font, 16 )
+  txt1.x = img1.x
+  txt1.y = img1.y + img1.height/2 + 20
+  txt1:setTextColor( color.red, color.green, color.blue )
+  screen:insert( txt1 )
+  txtItemTitles[#txtItemTitles + 1] = txt1
+
+  local img2 = display.newImageRect( item2.thumbnail.filename, system.DocumentsDirectory, item2.thumbnail.width, item2.thumbnail.height )
+  img2.x = 586
+  img2.y = 120 + ((sectionIndex-1) * 316) + img2.height/2
+  img2.item = item2
+  screen:insert( img2 )
+  imgItemThumbnails[#imgItemThumbnails + 1] = img2
+
+  local txt2 = display.newText( item2.title, 0, 0, menu.theme.body.font, 16 )
+  txt2.x = img2.x
+  txt2.y = img2.y + img2.height/2 + 20
+  txt2:setTextColor( color.red, color.green, color.blue )
+  screen:insert( txt2 )
+  txtItemTitles[#txtItemTitles + 1] = txt2
+end
+
+local renderSections = function( theme, sections )
+  for i,section in ipairs( sections ) do
+    renderSectionHeader( theme, section, i )
+    renderSectionItems( theme, section, i )
+  end
+end
 ---------------------------------------------------------------------------------
 -- BEGINNING OF YOUR IMPLEMENTATION
 ---------------------------------------------------------------------------------
